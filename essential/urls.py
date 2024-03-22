158a6974063d415c5a0a3a15c9450571dab07550
timofey1826 <105168955+timofey1826@users.noreply.github.com>
2024-03-22 18:44:59 +0400

    perenos zavershon

--- essential/urls.py
+++ essential/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views, specific_comands
-
 urlpatterns = [
+    path('users/create',views.RegisterView.as_view()),#для регистрации пользователей
     path("users/login",views.LoginView.as_view()),#дяя входина 
     path("users/auth",views.UserView.as_view()),
     path('users/refresh',views.refresh_token),
-
+    
     path("user/<id>",views.user_managing),
     path("job_title/<id>",views.job_title_managing),
     path("project/<id>",views.project_managing),
--- essential/views.py
+++ essential/views.py
@@ -1,18 +1,14 @@
-
-from django.shortcuts import render
 from rest_framework.response import Response
-# Create your views here.
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
-from essential.models import JobTitle, Project, User
-from essential.serializer import JobTitleSerializer, UsersSerializer
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 import jwt,datetime
+
 from .basic_comands import db_get, db_create, db_update
+from .models import User, JobTitle, Project, Task, UserWithTask
+from .serializer import UsersSerializer, JobTitleSerializer, ProjectSerializer, TaskSerializer, UserWithTaskSerializer
+
 
 @api_view(['GET', 'POST', 'DELETE', 'PUT'])
 def user_managing(request, id):
@@ -74,6 +70,39 @@ def user_with_task_managing(request, id):
 
 
 
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UsersSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=201)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        data = request.data
+        login = data['login']
+        password = data['password']
+        user = User.objects.filter(login=login).first()
+        if user is None:
+            raise AuthenticationFailed('Нету такого пользователя')
+        if not user.password == password:
+            raise AuthenticationFailed('Неверный пароль')
+        payload = {
+            'user': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=10),
+            'iat': datetime.datetime.utcnow()
+        }
+        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+        response = Response()
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'message': 'Успешно логинизировлись',
+            'token': token
+        }
+        return response
+
+
 class UserView(APIView):
     def get(self, request):
 
@@ -86,9 +115,10 @@ class UserView(APIView):
             raise AuthenticationFailed('Токен истек')
         user = User.objects.filter(id=payload['user']).first()
         serialize = UsersSerializer(user)
-        return Response({'message': 'Ты аутетифицирован','token':token,'userData':serialize.data})
+        return Response({'message': 'Ты аутетифицирован', 'token': token, 'userData': serialize.data})
 
-@api_view([ 'POST'])
+
+@api_view(['POST'])
 def refresh_token(request):
     data = request.data
     token = data['token']
@@ -102,9 +132,8 @@ def refresh_token(request):
             new_payload = {'exp': datetime.datetime.now() + datetime.timedelta(days=10)}
             new_token = jwt.encode(new_payload, 'secret', algorithm='HS256')
             return Response({'message': 'Токен истек, но был обновлен', 'token': new_token.decode('utf-8')}, status=200)
-        
+
     except jwt.exceptions.DecodeError:
         return None
-
 
 
