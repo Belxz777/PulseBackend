248a200095a3f52298c5a618f461b1d83739cb2a
Roman <140193303+Belxz777@users.noreply.github.com>
2024-05-10 12:17:55 +0400

    instance

--- essential/serializer.py
+++ essential/serializer.py
@@ -102,7 +102,7 @@ class TaskSerializer(serializers.ModelSerializer):
         instance.hoursToAccomplish = validated_data.get('hoursToAccomplish', instance.hoursToAccomplish)
         instance.stageAt = validated_data.get('stageAt', instance.stageAt)
         instance.priority = validated_data.get('priority', instance.priority)
-        instance.workers.set(validated_data['workers'])
+        # instance.workers.set(validated_data['workers'])
         instance.save()
         return instance
 
